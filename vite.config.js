9f30f11d0586dccbc06ef938f296e2df200b7f4b
malek1977 <malek.arr2006@gmail.com>
2025-09-20 20:20:39 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,27 @@
-// vite.config.js
+// // vite.config.js
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import tailwindcss from 'tailwindcss'
+// import path from 'path'
+
+// export default defineConfig({
+//   plugins: [react(),
+//   base: '/skyicon-travel-system-/',
+//     react(),
+//     tailwindcss()
+//   ],
+//   resolve: {
+//     alias: {
+//       '@': path.resolve(__dirname, "./src"),
+//     },
+//   },
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from 'tailwindcss'
-import path from 'path'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),
-  base: '/skyicon-travel-system-/',
-    react(),
-    tailwindcss()
-  ],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, "./src"),
-    },
-  },
+  plugins: [react()],
+  base: '/skyicon-travel-system-/', // 👈 هذا السطر يحدد المسار الصحيح لمشروعك في GitHub Pages
 })
