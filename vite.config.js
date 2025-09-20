c073e8a4e0f2494feebd4c6618f3753b1e70f4b1
malek1977 <malek.arr2006@gmail.com>
2025-09-20 20:24:41 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/skyicon-travel-system-/', // 👈 هذا السطر يحدد المسار الصحيح لمشروعك في GitHub Pages
+  base: '/skyicon-travel-system-/',
 })
